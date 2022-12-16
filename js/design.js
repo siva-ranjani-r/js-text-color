3f8146047f177a73f6ab7dd9bc02c9585c739a21
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-16 22:04:38 +0530

    Adding js file.

--- js/design.js
+++ js/design.js
@@ -2,17 +2,12 @@ txt=document.createElement("input");
 txt.setAttribute("id", "siva");
 document.body.appendChild(txt);
 
-btn=document.createElement("Button");
-btn.innerHTML="click";
-document.body.appendChild(btn);
+document.getElementById("siva").addEventListener("mousedown", mouseDown);
 
-flex=document.createElement("p");
-flex.setAttribute("id", "ranjani");
-document.body.appendChild(flex);
+
+function mouseDown() {
 	
-btn.onclick=function()
-{
-	var para = document.getElementById("siva").value;
-	document.getElementById("ranjani").innerHTML = para;
-	document.getElementById("ranjani").style.color = para;
+	var para=document.getElementById("siva").value;
+	document.getElementById("siva").style.color=para;
 }
+
